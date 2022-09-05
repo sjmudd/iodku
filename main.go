b0eb9edc3b47c45f9a8b2b2e51468254cfbe11d2
Simon J Mudd <sjmudd@pobox.com>
2022-09-06 01:27:38 +0200

    Add a --summary option of all inserts

--- main.go
+++ main.go
@@ -35,11 +35,59 @@ const (
 	;`
 )
 
+// Result contains the result of a single insert attempt
+type Result struct {
+	started  time.Time
+	err      error
+	duration time.Duration
+}
+
+// durationMetrics returns the number of successful attempts and the minimum, maximum and average duration of the successful insert attempts
+func durationMetrics(results []Result) (int, time.Duration, time.Duration, time.Duration) {
+	var (
+		successful int
+		total      int64         // time.Duration
+		min        time.Duration = 1<<63 - 1
+		max        time.Duration
+	)
+
+	for _, v := range results {
+		if v.err == nil {
+			successful++
+		}
+		total += int64(v.duration)
+		if v.duration < min {
+			min = v.duration
+		}
+		if v.duration > max {
+			max = v.duration
+		}
+	}
+
+	return successful, min, max, time.Duration(total / int64(len(results)))
+}
+
+func printSummary(interval time.Duration, results []Result) {
+	log.Printf("Summary:")
+	log.Printf("- interval:   %v", interval)
+	log.Printf("- size:       %d", len(results))
+	if len(results) > 0 {
+		success, min, max, average := durationMetrics(results)
+		log.Printf("- successful: %d, %6.2f%%", success, 100.0*float64(success)/float64(len(results)))
+		log.Printf("- min:        %v", min)
+		log.Printf("- average:    %v", average)
+		log.Printf("- max:        %v", max)
+	}
+}
+
 func main() {
+	var results []Result
+
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
 
 	insertInterval := flag.String("insert-interval", "1s", "insert interval as understood by go")
 	count := flag.Int("count", -1, "number of inserts to perform, default -1 = infinite")
+	summary := flag.Bool("summary", false, "provide a summary when the insert count has been reached")
 	flag.Parse()
 
 	parsedInterval, err := time.ParseDuration(*insertInterval)
@@ -62,6 +110,7 @@ func main() {
 	}
 	defer db.Close()
 
+	startTime := time.Now()
 	for *count == -1 || *count > 0 {
 		func() {
 			started := time.Now()
@@ -74,11 +123,22 @@ func main() {
 			} else {
 				log.Printf("Error after %v: %v", elapsed, err)
 			}
+			results = append(
+				results,
+				Result{
+					started:  started,
+					err:      err,
+					duration: elapsed,
+				})
 		}()
 
 		time.Sleep(parsedInterval)
 		if *count != -1 {
 			*count--
 		}
+	}
+
+	if *summary {
+		printSummary(time.Since(startTime), results)
 	}
 }
